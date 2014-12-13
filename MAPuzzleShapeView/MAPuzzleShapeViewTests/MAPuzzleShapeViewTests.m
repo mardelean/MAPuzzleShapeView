0ca261af69921a328970fe48922b65dc5cbb5c9d
Madalina Ardelean <ardeleanmada1@gmail.com>
2014-12-13 18:51:15 +0200

    Folders renamed from MACustomShapeView to MAPuzzleShapeView and screenshots added.

--- MAPuzzleShapeView/MAPuzzleShapeViewTests/MAPuzzleShapeViewTests.m
+++ MAPuzzleShapeView/MAPuzzleShapeViewTests/MAPuzzleShapeViewTests.m
@@ -1,19 +1,19 @@
 //
-//  MACustomShapeViewTests.m
-//  MACustomShapeViewTests
+//  MAPuzzleShapeViewTests.m
+//  MAPuzzleShapeViewTests
 //
-//  Created by Madalina Ardelean on 12/9/14.
+//  Created by Madalina Ardelean on 12/13/14.
 //  Copyright (c) 2014 Madalina Ardelean. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface MACustomShapeViewTests : XCTestCase
+@interface MAPuzzleShapeViewTests : XCTestCase
 
 @end
 
-@implementation MACustomShapeViewTests
+@implementation MAPuzzleShapeViewTests
 
 - (void)setUp {
     [super setUp];
